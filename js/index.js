f4ce0f03d153002e03d8480b0da0a9a734a1d611
CalebAllard <allard.caleb@gmail.com>
2019-10-28 16:35:08 -0400

    finished about to main-cont > about

--- js/index.js
+++ js/index.js
@@ -40,3 +40,34 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//header nav and img
+let navItems = document.querySelectorAll('a');
+for (i = 0; i < navItems.length; i++){
+  navItems[i].innerText = siteContent.nav[`nav-item-${i + 1}`];
+}
+
+
+let ctaText = document.querySelector('.cta > .cta-text > h1');
+
+let textTemp = siteContent['cta']['h1'];
+textTemp = textTemp.split(' ');
+ctaText.innerHTML = textTemp[0] + "<br>" + textTemp[1] + "<br>" + textTemp[2];
+console.log(textTemp);
+// Cta section
+let ctaImage = document.getElementById('cta-img');
+ctaImage.setAttribute('src', siteContent.cta['img-src']);
+
+let ctaButton = document.querySelector('.cta > .cta-text button');
+ctaButton.innerText = siteContent.cta['button'];
+
+//main content section
+let featuresElement = document.querySelector('.main-content > .top-content > .text-content > h4');
+featuresElement.innerText = siteContent['main-content']['features-h4'];
+featuresElement = document.querySelector('.main-content > .top-content > .text-content > p');
+featuresElement.innerText = siteContent['main-content']['features-content'];
+
+let aboutElement = document.querySelector('.main-content > .top-content > .text-content:nth-child(2) > h4');
+aboutElement.innerText = siteContent['main-content']['about-h4'];
+aboutElement = document.querySelector('.main-content > .top-content > .text-content:nth-child(2) > p');
+aboutElement.innerText = siteContent['main-content']['about-content'];
